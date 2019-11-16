0aad24af34b5c01607b7e2249bc86812c24d7496
Nicolas Jacquemin <n.jacquemin1993@gmail.com>
2019-11-16 21:39:40 +0100

    Use GPIO listener rather than while loop AND improve text

--- counter.py
+++ counter.py
@@ -4,17 +4,35 @@ from PIL import Image, ImageDraw, ImageFont, ImageTk
 from datetime import datetime
 import numpy
 import sys, os
+import time
 if sys.version_info[0] == 2:  # the tkinter library changed it's name from Python 2 to 3.
     import Tkinter
     tkinter = Tkinter #I decided to use a library reference to avoid potential naming conflicts with people's programs.
     import tkFont
 else:
     import tkinter, tkinter.font as tkFont
-
 import RPi.GPIO as GPIO
 
-filepath = "/home/pi/counter/counter.txt"
+# Listener for GPIO
+def onButtonPress(channel):
+    global counter
+    global update
+    counter += 1
+    update = True
 
+# Setup variables
+filepath = "/home/pi/counter/counter.txt"
+update = False
+try:
+    with open(filepath) as f:
+        counter = int(f.readlines()[0])
+except:
+    counter = 0
+
+# Load background
+img = Image.open('/home/pi/counter/BackgroundCompteurShots.png')
+
+# Setup TKinter
 root = tkinter.Tk()
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 root.overrideredirect(1)
@@ -23,42 +41,37 @@ root.focus_set()
 canvas = tkinter.Canvas(root,width=w,height=h)
 canvas.pack()
 canvas.configure(background='black')
-canvasImage = None
-
-# Load background
-img = Image.open('/home/pi/counter/BackgroundCompteurShots.png')
 imgWidth, imgHeight = img.size
 ratio = min(float(w)/imgWidth, float(h)/imgHeight)
 imgWidth = int(imgWidth*ratio)
 imgHeight = int(imgHeight*ratio)
 img = img.resize((imgWidth,imgHeight), Image.ANTIALIAS)
 image = ImageTk.PhotoImage(img)
-canvasImage = canvas.create_image(w/2,h/2,image=image)
-counterText = canvas.create_text(w/2, h/2, text=" 0 ", fill="#fab150", font=("SYEMOX italic", 140,"normal" ))
-canvas.create_text(w/2, 3*h/4, text=u"shots servis depuis le début de la soirée", fill="#fab150", font=("SYEMOX italic", 90,"normal" ))
+canvas.create_image(w/2,h/2,image=image)
+counterText = canvas.create_text(w/2, h/2, text=" {0} ".format(counter), fill="#fab150", font=("SYEMOX italic", 150,"normal"))
+canvas.create_text(w/2, 3*h/4, text=u"shots servis", fill="#fab150", font=("SYEMOX italic", 110, "normal"))
+root.update_idletasks()
+root.update()
 
-# Load counter
+# setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(23, GPIO.FALLING, callback=onButtonPress, bouncetime=120)
+
+# Infinite loop until keyboard interrupt
 try:
-    with open(filepath) as f:
-        counter = int(f.readlines()[0])
-except:
-    counter = 0
+    while True:
+        time.sleep(0.1)
+        if update:
+            update = False
+            canvas.itemconfigure(counterText, text=" {} ".format(counter))
+            root.update_idletasks()
+            root.update()
+            with open(filepath, "w") as f:
+                f.write("{0}".format(counter))
 
-while True:
-    canvas.itemconfigure(counterText, text=" {} ".format(counter))
-    root.update_idletasks()
-    root.update()
-    # wait for gpio
-    GPIO.setmode(GPIO.BCM)
-    # GPIO 23 set up as input. It is pulled up to stop false signals
-    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    # connect GPIO port 23 (pin 16) to GND (pin 6)
-    try:
-        GPIO.wait_for_edge(23, GPIO.FALLING)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        exit()
-    GPIO.cleanup()
-    counter += 1
-    with open(filepath, "w") as f:
-        f.write("{0}".format(counter))
+except KeyboardInterrupt:
+    pass
+
+# Clean exit
+GPIO.cleanup()
