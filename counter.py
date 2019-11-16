66573dfda0148877434f42e0381eca2c31475df4
Nicolas Jacquemin <n.jacquemin1993@gmail.com>
2019-11-16 13:36:52 +0100

    Improve speed

--- counter.py
+++ counter.py
@@ -23,64 +23,27 @@ canvas.pack()
 canvas.configure(background='black')
 canvasImage = None
 
-print(tkFont.names())
-
-
-def showPIL(pilImage):
-    global canvasImage
-    print("Enter show pil: {0}".format(datetime.now()))
-    imgWidth, imgHeight = pilImage.size
-    print(imgWidth, imgHeight)
-    # resize photo to full screen
-    ratio = min(float(w)/imgWidth, float(h)/imgHeight)
-    imgWidth = int(imgWidth*ratio)
-    imgHeight = int(imgHeight*ratio)
-    print(imgWidth, imgHeight)
-    print("Resize: {0}".format(datetime.now()))
-#    pilImage = pilImage.resize((imgWidth,imgHeight), Image.ANTIALIAS)
-    print("Photo image: {0}".format(datetime.now()))
-    image = ImageTk.PhotoImage(pilImage)
-    print("Create image: {0}".format(datetime.now()))
-    if canvasImage == None:
-        canvasImage = canvas.create_image(w/2,h/2,image=image)
-    else:
-        canvas.itemconfig(canvasImage, image=image)
-    label = canvas.create_text(w/2, h/2, text="Test", fill="#ffab15", font=("SYEMOX italic",50 ,"bold" ))
-    print("Update idle: {0}".format(datetime.now()))
-    root.update_idletasks()
-    print("Update: {0}".format(datetime.now()))
-    root.update()
-    print("Quit show pil: {0}".format(datetime.now()))
+#Load background
+img = Image.open('BackgroundCompteurShots.png')
+imgWidth, imgHeight = img.size
+ratio = min(float(w)/imgWidth, float(h)/imgHeight)
+imgWidth = int(imgWidth*ratio)
+imgHeight = int(imgHeight*ratio)
+img = img.resize((imgWidth,imgHeight), Image.ANTIALIAS)
+image = ImageTk.PhotoImage(img)
+canvasImage = canvas.create_image(w/2,h/2,image=image)
+counterText = canvas.create_text(w/2, h/2, text=" 0 ", fill="#fab150", font=("SYEMOX italic", 140,"normal" ))
+canvas.create_text(w/2, 3*h/4, text=u"shots servis depuis le début de la soirée", fill="#fab150", font=("SYEMOX italic", 90,"normal" ))
 
 counter = 0
 
 while True:
-    print("Prepare image: {0}".format(datetime.now()))
-    img = Image.open('BackgroundCompteurShots.png')
-    number = str(counter)
-    text = u"shots servis depuis le début de la soirée"
-
-    fnt_n = ImageFont.truetype('./Syemox.ttf', 150)
-    fnt_t = ImageFont.truetype('./Syemox.ttf', 120)
-    d = ImageDraw.Draw(img)
-    width_n, height_n = d.textsize(number, font=fnt_n)
-    width_t, height_t = d.textsize(text, font=fnt_t)
-    center_x = 1920 / 2
-    center_y = 650
-    margin_y = 70
-    color = (250, 177, 80)
-    pos_n_x = center_x - width_n / 2
-    pos_n_y = center_y - (height_n + height_t + margin_y) / 2
-    pos_t_x = center_x - width_t / 2
-    pos_t_y = pos_n_y + height_n + margin_y
-    d.text((pos_n_x, pos_n_y), number, font=fnt_n, fill=color)
-    d.text((pos_t_x, pos_t_y), text, font=fnt_t, fill=color)
-    showPIL(img)
+    canvas.itemconfigure(counterText, text=" {} ".format(counter))
+    root.update_idletasks()
+    root.update()
     # wait for gpio
-    print("Config GPIO: {0}".format(datetime.now()))
     GPIO.setmode(GPIO.BCM)
     # GPIO 23 set up as input. It is pulled up to stop false signals
-    print("Setup GPIO: {0}".format(datetime.now()))
     GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     # connect GPIO port 23 (pin 16) to GND (pin 6)
     try:
